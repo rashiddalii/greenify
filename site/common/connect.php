429a3c671d2b0a6985791b7c4fe8c8b38891c563
rashidali18 <mrashidali7541@gmail.com>
2025-02-01 19:21:33 +0500

    db update

--- site/common/connect.php
+++ site/common/connect.php
@@ -1,2 +1,2 @@
 <?php
-$con = mysqli_connect("sql308.infinityfree.com","if0_38221027","P7auohEJ9ZG1Xf","if0_38221027_product_listing");
+$con = mysqli_connect("localhost","root","","product_listing");
